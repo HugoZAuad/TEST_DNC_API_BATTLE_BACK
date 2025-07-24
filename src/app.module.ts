a0c5b9b93fb344a5fa02630e257b2b91b784e64c
HugoZAuad <hugozeymer@hotmail.com>
2025-07-24 15:03:23 -0300

    fix: incluido o modulo de arena no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { PlayerModule } from './modules/player/player.module';
 import { MonsterModule } from './modules/monster/monster.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-
 import { BattleModule } from './modules/battle/battle.module';
+import { ArenaModule } from './modules/arena/arena.module';
 
 @Module({
-  imports: [PlayerModule, MonsterModule, BattleModule],
+  imports: [PlayerModule, MonsterModule, BattleModule, ArenaModule],
   controllers: [AppController],
   providers: [
     AppService,
