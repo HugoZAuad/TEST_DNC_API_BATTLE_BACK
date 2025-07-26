57f683a8f4716bae517b03c7c1fc885654b46a16
HugoZAuad <hugozeymer@hotmail.com>
2025-07-26 11:57:26 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { msgPersona } from './common/pipes/msgPersona.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'http://localhost:5173',
+  app.enableCors({origin: 'http://localhost:5174',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,});
   app.useGlobalPipes(msgPersona());
