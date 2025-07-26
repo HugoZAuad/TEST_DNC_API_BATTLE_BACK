12cadc519038f48abb2138bb94dab36977690c88
HugoZAuad <hugozeymer@hotmail.com>
2025-07-26 11:51:56 -0300

    Fix: configuração cors feita para rodar no front

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionMiddleware } from './common/middlewares/http-exception.middleware';
@@ -6,6 +5,9 @@ import { msgPersona } from './common/pipes/msgPersona.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,});
   app.useGlobalPipes(msgPersona());
   app.use(new HttpExceptionMiddleware().use);
   await app.listen(process.env.PORT ?? 3000);
