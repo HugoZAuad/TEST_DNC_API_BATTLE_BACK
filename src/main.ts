8a66eeee1ab85550d746f686fcebb9814cf4c629
HugoZAuad <hugozeymer@hotmail.com>
2025-07-27 18:15:53 -0300

    feat: inserido o link da apalicação

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { msgPersona } from './common/pipes/msgPersona.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['http://localhost:5173', 'http://localhost:5174'],
+  app.enableCors({origin: ['http://localhost:5173', 'http://localhost:5174', 'https://test-dnc-api-battle-front.vercel.app/'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,});
   app.useGlobalPipes(msgPersona());
