8efb016cd2773a79a5e09768baee31016a8d1502
HugoZAuad <hugozeymer@hotmail.com>
2025-07-27 18:35:51 -0300

    fix: novo arquivo cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,25 @@ import { msgPersona } from './common/pipes/msgPersona.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: ['http://localhost:5173', 'http://localhost:5174', 'https://test-dnc-api-battle-front.vercel.app/*'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,});
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://test-dnc-api-battle-front.vercel.app',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(msgPersona());
   app.use(new HttpExceptionMiddleware().use);
   await app.listen(process.env.PORT ?? 3000);
